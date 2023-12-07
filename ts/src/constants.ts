32280f6450507bbef0954202fe180a05ae58a906
jtang-aurory <jonathan.tang@aurory.io>
2023-12-07 15:25:31 -0800

    Update tactics adv map format

--- ts/src/constants.ts
+++ ts/src/constants.ts
@@ -4,16 +4,16 @@ export const GLITCHED_RANGE_START = 5;
 export const SCHIMMERING_RANGE_START = 95;
 
 export const TACTICS_ADV_NAMES_MAP: Record<string, string> = {
-  Nefty_Bitebit: 'bitebit',
-  Nefty_Dipking: 'dipking',
-  Nefty_Dinobit: 'dinobit',
-  Nefty_ShibaIgnite: 'shiba ignite',
-  Nefty_Zzoo: 'zzoo',
-  Nefty_Blockchoy: 'block choy',
-  Nefty_Number9: 'number 9',
-  Nefty_Axobubble: 'axobubble',
-  Nefty_Unika: 'unika',
-  Nefty_Chocomint: 'chocomint',
-  Nefty_Cybertooth: 'cybertooth',
-  Nefty_Wassie: 'wassie',
+  Nefty_Bitebit: 'id_bitebit',
+  Nefty_Dipking: 'id_dipking',
+  Nefty_Dinobit: 'id_dinobit',
+  Nefty_ShibaIgnite: 'id_shiba ignite',
+  Nefty_Zzoo: 'id_zzoo',
+  Nefty_Blockchoy: 'id_block choy',
+  Nefty_Number9: 'id_number 9',
+  Nefty_Axobubble: 'id_axobubble',
+  Nefty_Unika: 'id_unika',
+  Nefty_Chocomint: 'id_chocomint',
+  Nefty_Cybertooth: 'id_cybertooth',
+  Nefty_Wassie: 'id_wassie',
 };
