607f8af3a60e85bb25f5e6a047af61a96dfcba3f
Levani Tevdoradze <12980455+tevdoradze@users.noreply.github.com>
2024-06-21 10:33:30 +0400

    Update read dna script

--- ts/scripts/read-dna.ts
+++ ts/scripts/read-dna.ts
@@ -3,7 +3,7 @@ import { DNAFactory } from '../src';
 function run() {
   const dna = process.argv[2];
   const df = new DNAFactory();
-  const data = df.parse(dna, '3.2.0');
+  const data = df.parse(dna);
   debugger;
   console.log(data);
 }
